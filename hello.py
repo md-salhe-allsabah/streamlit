4d5bcc14df90518c4520ad1c16b9ebee502e353d
Allsabah <mdsalheallsabah.code@gmail.com>
2026-09-24 18:06:54 +0530

    added JUT Ranchi syllabus urls

--- hello.py
+++ hello.py
@@ -3,4 +3,16 @@ import streamlit as st
 st.title('Hello World')
 st.divider()
 
-st.write("Hi i'm allsabah, Hopefully this streamlit app is hosted successfully on the streamlit cloud. I'm intended to dump here some of my important and frequently used resource links as my college and universities sites get crashed every alternate week.")
+with st.container(border=True):
+    st.write("Hi I'm allsabah, Hopefully this streamlit app is hosted successfully on the streamlit cloud. I'm intended to dump here some of my important and frequently used resource links as my college and universities sites get crashed every alternate week.")
+
+
+st.subheader('JUT Ranchi BTech syllabus')
+with st.container(border=True):
+    import pandas as pd
+    from url import urls
+    st.dataframe(pd.DataFrame(urls.items(), columns=['Years', 'Link']),column_config={
+        'Link': st.column_config.LinkColumn('Google Drive Link')
+    },
+    hide_index=True,
+    )
---
+++ url.py
@@ -0,0 +1,7 @@
+urls = {
+    '1st' : 'https://drive.google.com/drive/folders/1wtnTgC4JjlHL2SAFB4D1bdB38NZ3B7i9',
+
+    '2nd' : 'https://drive.google.com/drive/folders/1bq1C-wTlWx5Wjnf32gaY3iOGDX1CevL4' ,
+
+    '3rd and 4th' : 'https://drive.google.com/drive/folders/1Uk6fh_NjgAPrx8NGMppjJOnj4bD4-Cbo'
+}
