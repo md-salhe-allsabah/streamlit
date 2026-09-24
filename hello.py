27a2461953d00ffb2b00befcdb431757307c35d7
Allsabah <mdsalheallsabah.code@gmail.com>
2026-09-24 18:29:43 +0530

    trying to remove the footer

--- hello.py
+++ hello.py
@@ -1,15 +1,5 @@
 import streamlit as st
 
-st.html(
-    """
-    <style>
-        header {visibility: hidden;}
-        footer {visibility: hidden;}
-        #MainMenu {visibility: hidden;}
-    </style>
-    """
-)
-
 st.title("Hello World")
 st.divider()
 
@@ -18,17 +8,18 @@ with st.container(border=True):
         "Hi I'm allsabah, Hopefully this streamlit app is hosted successfully on the streamlit cloud. I'm intended to dump here some of my important and frequently used resource links as my college and universities sites get crashed every alternate week."
     )
 
+st.divider()
 
 st.subheader("JUT Ranchi BTech syllabus")
-with st.container(border=True):
-    import pandas as pd
 
-    from url import urls
+import pandas as pd
 
-    st.dataframe(
-        pd.DataFrame(urls.items(), columns=["Years", "Link"]),
-        column_config={"Link": st.column_config.LinkColumn("Google Drive Link")},
-        hide_index=True,
-    )
+from url import urls
+
+st.dataframe(
+    pd.DataFrame(urls.items(), columns=["Years", "Link"]),
+    column_config={"Link": st.column_config.LinkColumn("Google Drive Link")},
+    hide_index=True,
+)
     
     
